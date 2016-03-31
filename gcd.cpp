e76522bf1cab45f381ebd7d6bb9cf70cf129d188
jg75 <james@amberengine.com>
2016-03-31 14:06:07 -0400

    here is some stuff

---
+++ gcd.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int GCD(int, int);
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+
+    cout << GCD(a, b) << endl;
+
+    return 0;
+}
+
+int GCD(int a, int b)
+{
+    if (b < a)
+    {
+        return GCD(b, a);
+    }
+
+    int r = b % a;
+
+    if (r == 0)
+    {
+        return a;
+    }
+
+    return GCD(r, a);
+}
